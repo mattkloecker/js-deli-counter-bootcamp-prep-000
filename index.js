c88f7d74d0416b31798b99724087991921721a19
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-08-29 13:21:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, name){
 function nowServing(katzDeliLine){
   if (katzDeliLine.length>0) {
    return `Currently serving ${katzDeliLine[0]}.`;
-   katzDeliLine.shift(0);
+   katzDeliLine=katzDeliLine.shift(0);
   } else {
     return "There is nobody waiting to be served!"
   }
