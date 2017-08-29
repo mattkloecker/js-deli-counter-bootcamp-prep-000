85ab4eae5b5415e34f20e3a95b94ea46d4779693
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-08-29 13:34:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   var printout = [];
-  if (katzDeliLine.length=0) {
+  if (katzDeliLine.length===0) {
     return "The line is currently empty."
   } else {
   for (var i = 0; i < katzDeliLine.length; i++) {
